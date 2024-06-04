385df782c5a1bdb29ae35817385e71f4fcc7961d
AidenTran15 <aidenkiettran@gmail.com>
2024-06-05 09:45:11 +1000

    updated

--- frontend/src/components/ProductDetail/ProductDetail.js
+++ frontend/src/components/ProductDetail/ProductDetail.js
@@ -11,7 +11,7 @@ const ProductDetail = () => {
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await axios.get(`http://localhost:3001/products/${productId}`);
+        const response = await axios.get(`http://http://13.210.66.41/:3001/products/${productId}`);
         setProduct(response.data);
       } catch (error) {
         console.error('Error fetching product:', error);
